a8aa7a790fd5bfa713be8d68ccf1632463b221da
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2023-02-18 18:36:25 +0200

    Unify Demo and Ireland model and run with same script

--- utils/run_demos.py
+++ utils/run_demos.py
@@ -1,18 +1,16 @@
 import os
 import shutil
 import subprocess
+import sys
 from tabulate import tabulate
 import time
 
-# check WD
-# mkdir demos-csv
 
-
-def run_demo_model(demo_num):
-    xl_folder = f"demos-xlsx/DemoS_{demo_num:03d}"
-    dd_folder = f"demos-dd/DD-DemoS_{demo_num:03d}/"
-    csv_folder = f"demos-csv/DemoS_{demo_num:03d}"
-    out_folder = f"demos-out/DemoS_{demo_num:03d}"
+def run_benchmark(benchmarks_folder, benchmark_name):
+    xl_folder = os.path.join(benchmarks_folder, "xlsx", benchmark_name)
+    dd_folder = os.path.join(benchmarks_folder, "dd", benchmark_name)
+    csv_folder = os.path.join(benchmarks_folder, "csv", benchmark_name)
+    out_folder = os.path.join(benchmarks_folder, "out", benchmark_name)
 
     # First convert ground truth DD to csv, if we haven't already
     if not os.path.exists(csv_folder):
@@ -44,6 +42,8 @@ def run_demo_model(demo_num):
         ["python", "times_excel_reader.py"] + args, capture_output=True, text=True
     )
     runtime = time.time() - start
+    with open(os.path.join(out_folder, f"benchmark_name.out"), "w") as f:
+        f.write(res.stdout)
 
     if res.returncode == 0:
         lastline = res.stdout.splitlines()[-1].split(" ")
@@ -54,8 +54,16 @@ def run_demo_model(demo_num):
 
 
 if __name__ == "__main__":
+    assert len(sys.argv) == 2
+    benchmarks_folder = sys.argv[1]
+
+    # Each benchmark is a directory in the benchmarks/xlsx/ folder:
+    benchmarks = next(os.walk(os.path.join(benchmarks_folder, "xlsx")))[1]
+
     results = []
-    for i in range(1, 13):
-        results.append((i, *run_demo_model(i)))
+    for benchmark_name in benchmarks:
+        results.append(
+            (benchmark_name, *run_benchmark(benchmarks_folder, benchmark_name))
+        )
         print(".", end="", flush=True)
     print("\n" + tabulate(results, headers=["Demo", "Time", "Result"]) + "\n")
