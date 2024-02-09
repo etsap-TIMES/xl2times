871fcc571d84f33caf51cfafe1ce3c7677f6fea8
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2024-02-09 12:11:42 +0000

    Try to fix ReadTheDocs build errors (#178)

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ autodoc_class_signature = "separated"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_book_theme"
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 html_theme_options = {
     "repository_url": "https://github.com/etsap-TIMES/xl2times",
