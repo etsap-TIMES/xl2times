etsap-TIMES/xl2times

commit 7632912bf1b744230091e33c8d48df7f345df3a2

Add logging with loguru (#190)
Added logging via loguru - replaced all `print("WARNING:...` with
`logger.warning(`, `print(` with `logger.info(` etc.

Added utils.get_logger() for entrypoints, to hide configuration details
(and make it easier to replace with other logging lib later if needed).
Defaults to logging to console and a file named with the entrypoint
(e.g. `run_benchmarks.log` or `xl2times.log`)

Added poethepoet (poe for short) for adding make-like shortcuts into
pyproject.toml, e.g. `poe benchmark Ireland`, `poe lint`

utils/run_benchmarks.py
@@ -1,34 +1,41 @@
 import argparse
 import os
-from collections import namedtuple
-from concurrent.futures import ProcessPoolExecutor
-from functools import partial
-import git
-from os import path, symlink
-import pandas as pd
-from re import match
+import re
 import shutil
 import subprocess
 import sys
-from tabulate import tabulate
 import time
+from collections import namedtuple
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+from os import path, symlink
 from typing import Any, Tuple
-import yaml
 
+import git
+import pandas as pd
+import yaml
+from loguru import logger
+from tabulate import tabulate
+
+from xl2times import utils
 from xl2times.utils import max_workers
 
+logger = utils.get_logger()
 
-def parse_result(lastline):
-    m = match(
-        r"(\d+\.\d)\% of ground truth rows present in output \((\d+)/(\d+)\)"
-        r", (\d+) additional rows",
-        lastline,
+
+def parse_result(output: str) -> Tuple[float, int, int]:
+    # find pattern in multiline string
+    m = re.findall(
+        r"(\d+\.\d)% of ground truth rows present in output \((\d+)/(\d+)\), (\d+) additional rows",
+        output,
+        flags=re.MULTILINE,
     )
-    if not m:
-        print(f"ERROR: could not parse output of run:\n{lastline}")
+    if not m or len(m) == 0:
+        logger.error(f"could not parse output of run:\n{output}")
         sys.exit(2)
     # return (accuracy, num_correct_rows, num_additional_rows)
-    return (float(m.groups()[0]), int(m.groups()[1]), int(m.groups()[3]))
+    m = m[0]
+    return (float(m[0]), int(m[1]), int(m[2]))
 
 
 def run_gams_gdxdiff(
@@ -59,13 +66,13 @@ def run_gams_gdxdiff(
         text=True,
     )
     if res.returncode != 0:
-        print(res.stdout)
-        print(res.stderr if res.stderr is not None else "")
-        print(f"ERROR: GAMS failed on {benchmark['name']}")
+        logger.info(res.stdout)
+        logger.info(res.stderr if res.stderr is not None else "")
+        logger.error(f"GAMS failed on {benchmark['name']}")
         sys.exit(3)
     if "error" in res.stdout.lower():
-        print(res.stdout)
-        print(f"ERROR: GAMS errored on {benchmark['name']}")
+        logger.info(res.stdout)
+        logger.error(f"GAMS errored on {benchmark['name']}")
         return "Error running GAMS"
 
     # Run GAMS on ground truth:
@@ -90,13 +97,13 @@ def run_gams_gdxdiff(
         text=True,
     )
     if res.returncode != 0:
-        print(res.stdout)
-        print(res.stderr if res.stderr is not None else "")
-        print(f"ERROR: GAMS failed on {benchmark['name']} ground truth")
+        logger.info(res.stdout)
+        logger.info(res.stderr if res.stderr is not None else "")
+        logger.error(f"GAMS failed on {benchmark['name']} ground truth")
         sys.exit(4)
     if "error" in res.stdout.lower():
-        print(res.stdout)
-        print(f"ERROR: GAMS errored on {benchmark['name']}")
+        logger.info(res.stdout)
+        logger.error(f"GAMS errored on {benchmark['name']}")
         return "Error running GAMS on ground truth"
 
     # Run gdxdiff to compare
@@ -113,8 +120,8 @@ def run_gams_gdxdiff(
         text=True,
     )
     if verbose:
-        print(res.stdout)
-        print(res.stderr if res.stderr is not None else "")
+        logger.info(res.stdout)
+        logger.info(res.stderr if res.stderr is not None else "")
     if res.returncode != 0:
         return f"Diff ({len(res.stdout.splitlines())})"
 
@@ -154,8 +161,8 @@ def run_benchmark(
             if res.returncode != 0:
                 # Remove partial outputs
                 shutil.rmtree(csv_folder, ignore_errors=True)
-                print(res.stdout)
-                print(f"ERROR: dd_to_csv failed on {benchmark['name']}")
+                logger.info(res.stdout)
+                logger.error(f"dd_to_csv failed on {benchmark['name']}")
                 sys.exit(5)
         else:
             # If debug option is set, run as a function call to allow stepping with a debugger.
@@ -164,7 +171,7 @@ def run_benchmark(
             main([dd_folder, csv_folder])
 
     elif not path.exists(csv_folder):
-        print(f"ERROR: --skip_csv is true but {csv_folder} does not exist")
+        logger.error(f"--skip_csv is true but {csv_folder} does not exist")
         sys.exit(6)
 
     # Then run the tool
@@ -203,19 +210,19 @@ def run_benchmark(
 
     if verbose:
         line = "-" * 80
-        print(f"\n{line}\n{benchmark['name']}\n{line}\n\n{res.stdout}")
-        print(res.stderr if res.stderr is not None else "")
+        logger.info(f"\n{line}\n{benchmark['name']}\n{line}\n\n{res.stdout}")
+        logger.info(res.stderr if res.stderr is not None else "")
     else:
-        print(".", end="", flush=True)
+        logger.info(".", end="", flush=True)
 
     if res.returncode != 0:
-        print(res.stdout)
-        print(f"ERROR: tool failed on {benchmark['name']}")
+        logger.info(res.stdout)
+        logger.error(f"tool failed on {benchmark['name']}")
         sys.exit(7)
     with open(path.join(out_folder, "stdout"), "w") as f:
         f.write(res.stdout)
 
-    (accuracy, num_correct, num_additional) = parse_result(res.stdout.splitlines()[-1])
+    (accuracy, num_correct, num_additional) = parse_result(res.stdout)
 
     if run_gams:
         dd_res = run_gams_gdxdiff(benchmark, times_folder, dd_folder, out_folder)
@@ -236,7 +243,7 @@ def run_all_benchmarks(
     verbose=False,
     debug: bool = False,
 ):
-    print("Running benchmarks", end="", flush=True)
+    logger.info("Running benchmarks", end="", flush=True)
     headers = ["Benchmark", "Time (s)", "GDX Diff", "Accuracy", "Correct", "Additional"]
     run_a_benchmark = partial(
         run_benchmark,
@@ -250,10 +257,10 @@ def run_all_benchmarks(
 
     with ProcessPoolExecutor(max_workers=max_workers) as executor:
         results = list(executor.map(run_a_benchmark, benchmarks))
-    print("\n\n" + tabulate(results, headers, floatfmt=".1f") + "\n")
+    logger.info("\n\n" + tabulate(results, headers, floatfmt=".1f") + "\n")
 
     if skip_regression:
-        print("Skipping regression tests.")
+        logger.info("Skipping regression tests.")
         sys.exit(0)
 
     # The rest of this script checks regressions against main
@@ -271,7 +278,7 @@ def run_all_benchmarks(
         mybranch = repo.create_head("mybranch")
 
     if mybranch.name == "main":
-        print("Skipping regression tests as we're on main branch. Goodbye!")
+        logger.info("Skipping regression tests as we're on main branch. Goodbye!")
         sys.exit(0)
 
     if skip_main:
@@ -284,18 +291,20 @@ def run_all_benchmarks(
                 result = parse_result(f.readlines()[-1])
             # Use a fake runtime and GAMS result
             results_main.append((benchmark["name"], 999, "--", *result))
-        print(
+        logger.info(
             f"Skipped running on main. Using results from {path.join(benchmarks_folder, 'out-main')}"
         )
 
     else:
         if repo.is_dirty():
-            print("Your working directory is not clean. Skipping regression tests.")
+            logger.info(
+                "Your working directory is not clean. Skipping regression tests."
+            )
             sys.exit(8)
 
         # Re-run benchmarks on main
         repo.heads.main.checkout()
-        print("Running benchmarks on main", end="", flush=True)
+        logger.info("Running benchmarks on main", end="", flush=True)
         run_a_benchmark = partial(
             run_benchmark,
             benchmarks_folder=benchmarks_folder,
@@ -323,7 +332,7 @@ def run_all_benchmarks(
         )
         for ((b, t, f, a, c, d), (_, t0, f0, a0, c0, d0)) in zip(results, results_main)
     ]
-    print("\n\n" + tabulate(combined_results, headers, stralign="right") + "\n")
+    logger.info("\n\n" + tabulate(combined_results, headers, stralign="right") + "\n")
 
     # Checkout back to branch
     mybranch.checkout()
@@ -337,7 +346,7 @@ def run_all_benchmarks(
         how="outer",
     )
     if df.isna().values.any():
-        print(f"ERROR: number of benchmarks changed:\n{df}")
+        logger.error(f"number of benchmarks changed:\n{df}")
         sys.exit(9)
     accu_regressions = df[df["Correct"] < df["M Correct"]]["Benchmark"]
     addi_regressions = df[df["Additional"] > df["M Additional"]]["Benchmark"]
@@ -347,37 +356,36 @@ def run_all_benchmarks(
     main_time = df["M Time (s)"].sum()
     runtime_change = our_time - main_time
 
-    print(f"Total runtime: {our_time:.2f}s (main: {main_time:.2f}s)")
-    print(
-        f"Change in runtime (negative == faster): {runtime_change:+.2f}s ({100*runtime_change/main_time:+.1f}%)"
+    logger.info(f"Total runtime: {our_time:.2f}s (main: {main_time:.2f}s)")
+    logger.info(
+        f"Change in runtime (negative == faster): {runtime_change:+.2f}s ({100 * runtime_change / main_time:+.1f}%)"
     )
 
     our_correct = df["Correct"].sum()
     main_correct = df["M Correct"].sum()
     correct_change = our_correct - main_correct
-    print(
-        f"Change in correct rows (higher == better): {correct_change:+d} ({100*correct_change/main_correct:+.1f}%)"
+    logger.info(
+        f"Change in correct rows (higher == better): {correct_change:+d} ({100 * correct_change / main_correct:+.1f}%)"
     )
 
     our_additional_rows = df["Additional"].sum()
     main_additional_rows = df["M Additional"].sum()
     additional_change = our_additional_rows - main_additional_rows
-    print(
-        f"Change in additional rows: {additional_change:+d} ({100*additional_change/main_additional_rows:+.1f}%)"
+    logger.info(
+        f"Change in additional rows: {additional_change:+d} ({100 * additional_change / main_additional_rows:+.1f}%)"
     )
 
     if len(accu_regressions) + len(addi_regressions) + len(time_regressions) > 0:
-        print()
         if not accu_regressions.empty:
-            print(f"ERROR: correct rows regressed on: {', '.join(accu_regressions)}")
+            logger.error(f"correct rows regressed on: {', '.join(accu_regressions)}")
         if not addi_regressions.empty:
-            print(f"ERROR: additional rows regressed on: {', '.join(addi_regressions)}")
+            logger.error(f"additional rows regressed on: {', '.join(addi_regressions)}")
         if not time_regressions.empty:
-            print(f"ERROR: runtime regressed on: {', '.join(time_regressions)}")
+            logger.error(f"runtime regressed on: {', '.join(time_regressions)}")
         sys.exit(10)
     # TODO also check if any new tables are missing?
 
-    print("No regressions. You're awesome!")
+    logger.success("No regressions. You're awesome!")
 
 
 if __name__ == "__main__":
@@ -441,17 +449,17 @@ if __name__ == "__main__":
     benchmarks_folder = spec["benchmarks_folder"]
     benchmark_names = [b["name"] for b in spec["benchmarks"]]
     if len(set(benchmark_names)) != len(benchmark_names):
-        print("ERROR: Found duplicate name in benchmarks YAML file")
+        logger.error(f"Found duplicate name in benchmarks YAML file")
         sys.exit(11)
 
     if args.dd and args.times_dir is None:
-        print("ERROR: --times_dir is required when using --dd")
+        logger.error(f"--times_dir is required when using --dd")
         sys.exit(12)
 
     if args.run is not None:
         benchmark = next((b for b in spec["benchmarks"] if b["name"] == args.run), None)
         if benchmark is None:
-            print(f"ERROR: could not find {args.run} in {args.benchmarks_yaml}")
+            logger.error(f"could not find {args.run} in {args.benchmarks_yaml}")
             sys.exit(13)
 
         _, runtime, gms, acc, cor, add = run_benchmark(
@@ -463,7 +471,7 @@ if __name__ == "__main__":
             verbose=args.verbose,
             debug=args.debug,
         )
-        print(
+        logger.info(
             f"Ran {args.run} in {runtime:.2f}s. {acc}% ({cor} correct, {add} additional).\n"
             f"GAMS: {gms}"
         )

.gitignore
@@ -21,3 +21,4 @@ docs/_build/
 docs/api/
 .coverage
 /out.txt
+*.log

pyproject.toml
@@ -26,6 +26,7 @@ dependencies = [
     "pandas >= 2.1",
     "pyarrow",
     "tqdm",
+    "loguru"
 ]
 
 [project.optional-dependencies]
@@ -34,7 +35,8 @@ dev = [
     "pre-commit",
     "tabulate",
     "pytest",
-    "pytest-cov"
+    "pytest-cov",
+    "poethepoet"
 ]
 
 [project.urls]
@@ -50,3 +52,12 @@ xl2times = "xl2times.__main__:main"
 filterwarnings = ["ignore::DeprecationWarning", "ignore::UserWarning", "ignore::FutureWarning"]
 # show output, print test coverage report
 addopts = '-s --durations=0 --durations-min=5.0 --tb=native --cov-report term --cov-report html --cov=xl2times --cov=utils'
+
+[tool.poe.tasks]
+# Automation of common dev tasks etc.
+# Run with: `poe <target>`, e,g. `poe lint` or `poe benchmark Ireland`.
+# See https://github.com/nat-n/poethepoet for details.
+benchmark = { cmd = "python utils/run_benchmarks.py benchmarks.yml --verbose --run", help = "Run a single benchmark.  Usage: poe benchmark <benchmark_name>" }
+benchmark_all = { shell = "python utils/run_benchmarks.py benchmarks.yml --verbose | tee out.txt", help = "Run the project", interpreter = "posix" }
+lint = { shell = "git add .pre-commit-config.yaml & pre-commit run", help = "Run pre-commit hooks", interpreter = "posix"}
+test = { cmd = "pytest", help = "Run unit tests with pytest" }

utils/dd_to_csv.py
@@ -8,6 +8,7 @@ from typing import Dict, List, Tuple, Union
 
 import numpy as np
 import pandas as pd
+from loguru import logger
 
 
 def parse_parameter_values_from_file(
@@ -179,7 +180,7 @@ def convert_dd_to_tabular(
     all_sets = defaultdict(list)
     all_parameters = defaultdict(list)
     for path in dd_files:
-        print(f"Processing path: {path}")
+        logger.info(f"Processing path: {path}")
         local_param_values, local_sets = parse_parameter_values_from_file(path)
 
         # merge params from file into global collection

xl2times/__main__.py
@@ -10,9 +10,11 @@ import time
 from typing import Dict, List
 
 from xl2times.utils import max_workers
-from . import datatypes
+from . import datatypes, utils
 from . import excel
 from . import transforms
+
+logger = utils.get_logger()
 
 
 def convert_xl_to_times(
@@ -27,7 +29,7 @@ def convert_xl_to_times(
     pickle_file = "raw_tables.pkl"
     if use_pkl and os.path.isfile(pickle_file):
         raw_tables = pickle.load(open(pickle_file, "rb"))
-        print(f"WARNING: Using pickled data not xlsx")
+        logger.warning(f"Using pickled data not xlsx")
     else:
         raw_tables = []
 
@@ -41,7 +43,7 @@ def convert_xl_to_times(
                 result = excel.extract_tables(f)
                 raw_tables.extend(result)
         pickle.dump(raw_tables, open(pickle_file, "wb"))
-    print(
+    logger.info(
         f"Extracted {len(raw_tables)} tables,"
         f" {sum(table.dataframe.shape[0] for table in raw_tables)} rows"
     )
@@ -119,7 +121,7 @@ def convert_xl_to_times(
         output = transform(config, input, model)
         end_time = time.time()
         sep = "\n\n" + "=" * 80 + "\n" if verbose else ""
-        print(
+        logger.info(
             f"{sep}transform {transform.__code__.co_name} took {end_time - start_time:.2f} seconds"
         )
         if verbose:
@@ -127,15 +129,15 @@ def convert_xl_to_times(
                 for table in sorted(
                     output, key=lambda t: (t.tag, t.filename, t.sheetname, t.range)
                 ):
-                    print(table)
+                    logger.info(table)
             elif isinstance(output, dict):
                 for tag, df in output.items():
                     df_str = df.to_csv(index=False, lineterminator="\n")
-                    print(f"{tag}\n{df_str}{df.shape}\n")
+                    logger.info(f"{tag}\n{df_str}{df.shape}\n")
         input = output
     assert isinstance(output, dict)
 
-    print(
+    logger.info(
         f"Conversion complete, {len(output)} tables produced,"
         f" {sum(df.shape[0] for df in output.values())} rows"
     )
@@ -164,7 +166,7 @@ def read_csv_tables(input_dir: str) -> Dict[str, DataFrame]:
 def compare(
     data: Dict[str, DataFrame], ground_truth: Dict[str, DataFrame], output_dir: str
 ) -> str:
-    print(
+    logger.info(
         f"Ground truth contains {len(ground_truth)} tables,"
         f" {sum(df.shape[0] for _, df in ground_truth.items())} rows"
     )
@@ -174,14 +176,14 @@ def compare(
         [f"{x} ({ground_truth[x].shape[0]})" for x in sorted(missing)]
     )
     if len(missing) > 0:
-        print(f"WARNING: Missing {len(missing)} tables: {missing_str}")
+        logger.warning(f"Missing {len(missing)} tables: {missing_str}")
 
     additional_tables = set(data.keys()) - set(ground_truth.keys())
     additional_str = ", ".join(
         [f"{x} ({data[x].shape[0]})" for x in sorted(additional_tables)]
     )
     if len(additional_tables) > 0:
-        print(f"WARNING: {len(additional_tables)} additional tables: {additional_str}")
+        logger.warning(f"{len(additional_tables)} additional tables: {additional_str}")
     # Additional rows starts as the sum of lengths of additional tables produced
     total_additional_rows = sum(len(data[x]) for x in additional_tables)
 
@@ -197,8 +199,8 @@ def compare(
             transformed_gt_cols = [col.split(".")[0] for col in gt_table.columns]
             data_cols = list(data_table.columns)
             if transformed_gt_cols != data_cols:
-                print(
-                    f"WARNING: Table {table_name} header incorrect, was"
+                logger.warning(
+                    f"Table {table_name} header incorrect, was"
                     f" {data_cols}, should be {transformed_gt_cols}"
                 )
 
@@ -211,8 +213,8 @@ def compare(
             total_additional_rows += len(additional)
             missing = gt_rows - data_rows
             if len(additional) != 0 or len(missing) != 0:
-                print(
-                    f"WARNING: Table {table_name} ({data_table.shape[0]} rows,"
+                logger.warning(
+                    f"Table {table_name} ({data_table.shape[0]} rows,"
                     f" {gt_table.shape[0]} GT rows) contains {len(additional)}"
                     f" additional rows and is missing {len(missing)} rows"
                 )
@@ -232,14 +234,14 @@ def compare(
         f", {total_additional_rows} additional rows"
     )
 
-    print(result)
+    logger.info(result)
     return result
 
 
 def produce_times_tables(
     config: datatypes.Config, input: Dict[str, DataFrame]
 ) -> Dict[str, DataFrame]:
-    print(
+    logger.info(
         f"produce_times_tables: {len(input)} tables incoming,"
         f" {sum(len(value) for (_, value) in input.items())} rows"
     )
@@ -247,8 +249,8 @@ def produce_times_tables(
     used_tables = set()
     for mapping in config.times_xl_maps:
         if not mapping.xl_name in input:
-            print(
-                f"WARNING: Cannot produce table {mapping.times_name} because"
+            logger.warning(
+                f"Cannot produce table {mapping.times_name} because"
                 f" {mapping.xl_name} does not exist"
             )
         else:
@@ -257,8 +259,8 @@ def produce_times_tables(
             # Filter rows according to filter_rows mapping:
             for filter_col, filter_val in mapping.filter_rows.items():
                 if filter_col not in df.columns:
-                    print(
-                        f"WARNING: Cannot produce table {mapping.times_name} because"
+                    logger.warning(
+                        f"Cannot produce table {mapping.times_name} because"
                         f" {mapping.xl_name} does not contain column {filter_col}"
                     )
                     # TODO break this loop and continue outer loop?
@@ -270,8 +272,8 @@ def produce_times_tables(
                 df["techgroup"] = df["techname"]
             if not all(c in df.columns for c in mapping.xl_cols):
                 missing = set(mapping.xl_cols) - set(df.columns)
-                print(
-                    f"WARNING: Cannot produce table {mapping.times_name} because"
+                logger.warning(
+                    f"Cannot produce table {mapping.times_name} because"
                     f" {mapping.xl_name} does not contain the required columns"
                     f" - {', '.join(missing)}"
                 )
@@ -298,8 +300,8 @@ def produce_times_tables(
 
     unused_tables = set(input.keys()) - used_tables
     if len(unused_tables) > 0:
-        print(
-            f"WARNING: {len(unused_tables)} unused tables: {', '.join(sorted(unused_tables))}"
+        logger.warning(
+            f"{len(unused_tables)} unused tables: {', '.join(sorted(unused_tables))}"
         )
 
     return result
@@ -409,7 +411,7 @@ def run(args) -> str | None:
     model = datatypes.TimesModel()
 
     if not isinstance(args.input, list) or len(args.input) < 1:
-        print(f"ERROR: expected at least 1 input. Got {args.input}")
+        logger.critical(f"expected at least 1 input. Got {args.input}")
         sys.exit(-1)
     elif len(args.input) == 1:
         assert os.path.isdir(args.input[0])
@@ -418,7 +420,7 @@ def run(args) -> str | None:
             for path in Path(args.input[0]).rglob("*")
             if path.suffix in [".xlsx", ".xlsm"] and not path.name.startswith("~")
         ]
-        print(f"Loading {len(input_files)} files from {args.input[0]}")
+        logger.info(f"Loading {len(input_files)} files from {args.input[0]}")
     else:
         input_files = args.input
 

xl2times/datatypes.py
@@ -6,6 +6,8 @@ import json
 import re
 from typing import Dict, Iterable, List, Set, Tuple
 from enum import Enum
+
+from loguru import logger
 from pandas.core.frame import DataFrame
 
 # ============================================================================
@@ -235,7 +237,7 @@ class Config:
             cat_to_tables[item["gams-cat"]].append(item["name"])
         unknown_cats = {item["gams-cat"] for item in table_info} - set(categories)
         if unknown_cats:
-            print(f"WARNING: Unknown categories in times-info.json: {unknown_cats}")
+            logger.warning(f"Unknown categories in times-info.json: {unknown_cats}")
         dd_table_order = chain.from_iterable(
             (sorted(cat_to_tables[c]) for c in categories)
         )
@@ -307,9 +309,9 @@ class Config:
                     break
                 (times, xl) = line.split(" = ")
                 (times_name, times_cols_str) = list(
-                    filter(None, re.split("\[|\]", times))
+                    filter(None, re.split(r"\[|\]", times))
                 )
-                (xl_name, xl_cols_str) = list(filter(None, re.split("\(|\)", xl)))
+                (xl_name, xl_cols_str) = list(filter(None, re.split(r"\(|\)", xl)))
                 times_cols = times_cols_str.split(",")
                 xl_cols = xl_cols_str.split(",")
                 filter_rows = {}
@@ -343,8 +345,8 @@ class Config:
                     dropped.append(line)
 
         if len(dropped) > 0:
-            print(
-                f"WARNING: Dropping {len(dropped)} mappings that are not yet complete"
+            logger.warning(
+                f"Dropping {len(dropped)} mappings that are not yet complete"
             )
         return mappings
 
@@ -370,8 +372,8 @@ class Config:
         tags = {to_tag(tag_info["tag_name"]) for tag_info in veda_tags_info}
         for tag in Tag:
             if tag not in tags:
-                print(
-                    f"WARNING: datatypes.Tag has an unknown Tag {tag} not in {veda_tags_file}"
+                logger.warning(
+                    f"datatypes.Tag has an unknown Tag {tag} not in {veda_tags_file}"
                 )
 
         valid_column_names = {}

xl2times/excel.py
@@ -1,3 +1,4 @@
+from loguru import logger
 from openpyxl import load_workbook
 from openpyxl.worksheet.cell_range import CellRange
 from typing import Dict, List
@@ -42,7 +43,7 @@ def extract_tables(filename: str) -> List[datatypes.EmbeddedXlTable]:
                         if len(parts) == 2:
                             uc_sets[parts[0].strip()] = parts[1].strip()
                         else:
-                            print(
+                            logger.info(
                                 f"WARNING: Malformed UC_SET in {sheet.title}, {filename}"
                             )
                     else:
@@ -60,7 +61,7 @@ def extract_tables(filename: str) -> List[datatypes.EmbeddedXlTable]:
 
     end_time = time.time()
     if end_time - start_time > 2:
-        print(f"Loaded {filename} in {end_time-start_time:.2f} seconds")
+        logger.info(f"Loaded {filename} in {end_time-start_time:.2f} seconds")
 
     return tables
 

commit b3e9b8fc1db4b2c5b920d32c24db910d6a678e2f

CI: fix bug in parsing additional lines (#195)
Fixes a bug in the parsing of additional lines by the regression tests
introduced in #190:

https://github.com/etsap-TIMES/xl2times/pull/190/files#diff-cca48a967a9c20b6fc94f3c37310d00cd4c05e08fea487fafe27ab770590ec2bR38

Which causes the test to use the total lines as additional lines:
<img width="900" alt="image"
src="https://github.com/etsap-TIMES/xl2times/assets/10712637/18387688-950c-4b06-a61d-9a6175ed1a0d">

I manually compared the additional rows to the last
[run](https://github.com/etsap-TIMES/xl2times/actions/runs/7930773425/job/21653701343)
on the main branch before the bug to ensure that there have been no
regressions in additional rows in the meanwhile. 🚀

utils/run_benchmarks.py
@@ -36,7 +36,7 @@ def parse_result(output: str) -> Tuple[float, int, int]:
         sys.exit(2)
     # return (accuracy, num_correct_rows, num_additional_rows)
     m = m[0]
-    return (float(m[0]), int(m[1]), int(m[2]))
+    return (float(m[0]), int(m[1]), int(m[3]))
 
 
 def run_gams_gdxdiff(