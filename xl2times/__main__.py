9a312964290e747f62d4d8c297a014c86447b0a5
Olexandr Balyk <ob@facilitate.energy>
2024-03-22 12:33:40 -0500

    Change the order of transforms

--- xl2times/__main__.py
+++ xl2times/__main__.py
@@ -99,11 +99,11 @@ def convert_xl_to_times(
         transforms.generate_dummy_processes,
         transforms.process_time_slices,
         transforms.process_transform_table_variants,
+        transforms.apply_tag_specified_defaults,
         transforms.process_transform_tables,
         transforms.process_tradelinks,
         transforms.process_processes,
         transforms.process_topology,
-        transforms.apply_tag_specified_defaults,
         transforms.process_flexible_import_tables,  # slow
         transforms.process_user_constraint_tables,
         transforms.process_commodity_emissions,
