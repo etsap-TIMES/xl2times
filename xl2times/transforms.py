1e2879c32c8d952ffc627218062ccdcab5c7c7bf
Olexandr Balyk <ob@facilitate.energy>
2025-01-24 18:42:14 -0500

    Correct the condition

--- xl2times/transforms.py
+++ xl2times/transforms.py
@@ -3261,10 +3261,8 @@ def apply_final_fixup(
         i_vt = (
             stock_index
             & (df["module_name"] == "BASE")
-            & (
-                (~df["source_filename"].str.contains("_SUP", case=False))
-                | (~df["source_filename"].str.contains("_UPS", case=False))
-            )
+            & (~df["source_filename"].str.contains("_SUP", case=False))
+            & (~df["source_filename"].str.contains("_UPS", case=False))
         )
         # Create (region, process) index for data defined in vt
         i_rp_vt = df[i_vt].set_index(["region", "process"]).index.drop_duplicates()
