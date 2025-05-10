ea21b6c884cdc990fabd09000714cbd578c97a70
Olexandr Balyk <ob@facilitate.energy>
2025-05-10 15:30:41 +0300

    Change module names assigned to demand scenarios

--- xl2times/datatypes.py
+++ xl2times/datatypes.py
@@ -137,7 +137,6 @@ class DataModule(str, Enum):
                 DataModule.base
                 | DataModule.sets
                 | DataModule.lma
-                | DataModule.demand
                 | DataModule.trade
                 | DataModule.syssettings
             ):
@@ -148,6 +147,11 @@ class DataModule(str, Enum):
                 )
             case DataModule.scen:
                 return re.sub("^SCEN_", "", PurePath(path).stem.upper())
+            case DataModule.demand:
+                if PurePath(path.lower()).match("dem_alloc+series.*"):
+                    return module_type.name.upper()
+                else:
+                    return re.sub("^SCENDEM_", "", PurePath(path).stem.upper())
             case None:
                 return None
 
